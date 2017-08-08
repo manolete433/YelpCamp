79380c3f24e9bb7e23a163e20db94791ef4029a1
Manuel Vazquez <manolete433@gmail.com>
2017-08-08 21:03:24 +0000

    update DBURL

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://manolete433:12345678@ds153652.mlab.com:53652/yelpcamp_manolete433");
+console.log(process.env.DATABASEURL);
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://manolete433:12345678@ds153652.mlab.com:53652/yelpcamp_manolete433");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
