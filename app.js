a4b991449e8be68df2725d8f334fb372acf33236
Manuel Vazquez <manolete433@gmail.com>
2017-08-08 16:25:19 +0000

    Add MLab connection string

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://manolete433:12345678@ds153652.mlab.com:53652/yelpcamp_manolete433");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
